e4cba8db286e7eb6e214663f7f8fe689e6e4e7bf
Alessandro Fior <73718633+alessandro-fior@users.noreply.github.com>
2022-08-15 13:49:30 +0200

    basic test
    basic test

---
+++ Fade.ino
@@ -0,0 +1,58 @@
+/*
+  Fade
+
+  This example shows how to fade an LED on pin 9 using the analogWrite()
+  function.
+
+  The analogWrite() function uses PWM, so if you want to change the pin you're
+  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
+  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
+*/
+int led  = 0;           
+int led1 = 1;           
+int led2 = 2;
+int led3 = 3;
+int led4 = 4;
+int led5 = 5;
+int led6 = 6;
+int led7 = 7;
+int led8 = 8;
+int led9 = 9;
+           
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // declare pin 9 to be an output:
+  pinMode(led, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // set the brightness of pin 9:
+  analogWrite(led, brightness);
+  analogWrite(led1, brightness);
+  analogWrite(led2, brightness);
+  analogWrite(led3, brightness);
+  analogWrite(led4, brightness);
+  analogWrite(led5, brightness);
+  analogWrite(led6, brightness);
+  analogWrite(led7, brightness);
+  analogWrite(led8, brightness);
+  analogWrite(led9, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  // wait for 30 milliseconds to see the dimming effect
+  delay(30);
+}
